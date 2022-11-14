a647f14803430eab35262142a3af0506573a655f
Mazen Muhammed Alsayed <115193793+mazenALsayed@users.noreply.github.com>
2022-11-14 20:01:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ import sys
 import math
 from tkinter import *
 from tkinter import messagebox
+import copy
 
 ROW_COUNT = 6
 COL_COUNT = 7
+scope_length = 4
+EMPTY = 0
 PLAYER = 0
 PLAYER_PIECE = 1
 AI = 1
@@ -29,14 +32,20 @@ def create_connect4():
 
 ## checks the last ROW if it's empty so that the move would be valid
 def move_is_valid(board, COL):
-    if board[ROW_COUNT - 1][col] == 0:
+    if board[ROW_COUNT - 1][COL] == 0:
         return True
 
+def valid_locations(board):
+    valid = []
+    for c in range(COL_COUNT):
+        if move_is_valid(board, c):
+            valid.append(c)
+    return valid
 
 ## check if the ROW is empty so that we know where the player can play
 def which_ROW(board, col):
     for r in range(ROW_COUNT):
-        if board[r][col] == 0:
+        if board[r][col] == EMPTY:
             return r
 
 
@@ -48,7 +57,7 @@ def flip_board(board):
     board = np.flipud(board)
 
 
-def game_done():
+def game_done(board):
     k = 0
     for i in range(COL_COUNT):
         if (board[5][i] == 0):
@@ -59,15 +68,69 @@ def game_done():
         return False
 
 
-def score(board):
-    pass
+def score_scope(scope, piece):
+    score_sum = 0
+    if piece == AI_PIECE:
+        opp_piece = PLAYER_PIECE
+    else:
+        opp_piece = AI_PIECE
+
+    if scope.count(piece) == 4:
+        score_sum += 100
+    elif scope.count(piece) == 3 and scope.count(EMPTY) == 1:
+        score_sum += 10
+    elif scope.count(piece) == 2 and scope.count(EMPTY) == 2:
+        score_sum += 5
+
+    if scope.count(opp_piece) == 3 and scope.count(EMPTY) == 1:
+        score_sum -= 10
+    return score_sum
+
+
+
+
+
+
+
+def score(board, piece):
+    score_sum = 0
+    #center dom
+    center = [int(i) for i in list(board[:, COL_COUNT//2])]
+    center_count = center.count(piece)
+    score_sum += center_count*5
+    #horizontal scores
+    for r in range(ROW_COUNT):
+        row = [int(i) for i in list(board[r,:])]
+        for c in range(COL_COUNT-3):
+            scope = row[c:c+scope_length]
+            score_sum += score_scope(scope, piece)
+    #vertical_scores
+    for c in range(COL_COUNT):
+        col = [int(i) for i in list(board[:,c])]
+        for r in range(ROW_COUNT-3):
+            scope = col[r:r+scope_length]
+            score_sum += score_scope(scope, piece)
+    #+ve diagonal scores
+    for r in range(ROW_COUNT-3):
+        for c in range(COL_COUNT-3):
+            scope = [board[r+i][c+i] for i in range(scope_length)]
+            score_sum += score_scope(scope, piece)
+    #-ve diagonal scores
+    for r in range(ROW_COUNT-3):
+        for c in range(COL_COUNT-3):
+            scope = [board[r+3-i][c+i] for i in range(scope_length)]
+            score_sum += score_scope(scope, piece)
+
+    return score_sum
+
 
 
 def final_score(board):
-    sum1 = 0
-    sum2 = 0
+    sum1 = 0 #player score
+    sum2 = 0 #ai score
     piece1 = PLAYER_PIECE
 
+    #vertical check
     for c in range(COL_COUNT - 3):
         for r in range(ROW_COUNT):
             if board[r][c] == board[r][c + 1] and board[r][c] == board[r][c + 2] and board[r][c] == board[r][c + 3]:
@@ -75,6 +138,7 @@ def final_score(board):
                     sum1 += 1
                 else:
                     sum2 += 1
+    #horizontal check
     for c in range(COL_COUNT):
         for r in range(ROW_COUNT - 3):
             if board[r][c] == board[r + 1][c] and board[r][c] == board[r + 2][c] and board[r][c] == board[r + 3][c]:
@@ -82,7 +146,7 @@ def final_score(board):
                     sum1 += 1
                 else:
                     sum2 += 1
-
+    #+ve diagonal check
     for c in range(COL_COUNT - 3):
         for r in range(ROW_COUNT - 3):
             if (board[r][c] == board[r + 1][c + 1] and board[r][c] == board[r + 2][c + 2] and board[r][c] ==
@@ -91,6 +155,7 @@ def final_score(board):
                     sum1 += 1
                 else:
                     sum2 += 1
+    # -ve diagonal check
     for c in range(COL_COUNT - 3):
         for r in range(3, ROW_COUNT):
             if (board[r][c] == board[r - 1][c + 1] and board[r][c] == board[r - 2][c + 2] and board[r][c] ==
@@ -101,6 +166,94 @@ def final_score(board):
                     sum2 += 1
 
     return sum1, sum2
+
+
+def minimax(board, depth, maxplayer):
+    locations = valid_locations(board)
+    if depth == 0 or game_done(board):
+        if game_done(board):
+            s1, s2 = final_score(board)
+            if s2 > s1:
+                return (None, 10000000000)
+            elif s1 > s2:
+                return (None, -10000000000)
+            else:
+                return (None, 0)
+        else: #depth is 0
+            return (None, score(board, AI_PIECE))
+    if maxplayer: #maxplayer
+        value = -math.inf
+        column = random.choice(locations)
+        for col in locations:
+            row = which_ROW(board, col)
+            temp = board.copy()
+            piece_drop(temp, row, col, AI_PIECE)
+            new_value = minimax(temp, depth-1, False)[1]
+            if new_value > value:
+                value = new_value
+                column = col
+        return column, value
+
+    else: #minplayer
+        value = math.inf
+        column = random.choice(locations)
+        for col in locations:
+            row = which_ROW(board, col)
+            temp = board.copy()
+            piece_drop(temp, row, col, PLAYER_PIECE)
+            new_value = minimax(temp, depth - 1, True)[1]
+            if new_value < value:
+                value = new_value
+                column = col
+        return column, value
+
+
+def minimax_pruning(board, depth, alpha, beta, maxplayer):
+    locations = valid_locations(board)
+    if depth == 0 or game_done(board):
+        if game_done(board):
+            s1, s2 = final_score(board)
+            if s2 > s1:
+                return (None, 10000000000)
+            elif s1 > s2:
+                return (None, -10000000000)
+            else:
+                return (None, 0)
+        else: #depth is 0
+            return (None, score(board, AI_PIECE))
+    if maxplayer: #maxplayer
+        value = -math.inf
+        column = random.choice(locations)
+        for col in locations:
+            row = which_ROW(board, col)
+            temp = board.copy()
+            piece_drop(temp, row, col, AI_PIECE)
+            new_value = minimax_pruning(temp, depth-1, alpha, beta, False)[1]
+            if new_value > value:
+                value = new_value
+                column = col
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        return column, value
+
+    else: #minplayer
+        value = math.inf
+        column = random.choice(locations)
+        for col in locations:
+            row = which_ROW(board, col)
+            temp = board.copy()
+            piece_drop(temp, row, col, PLAYER_PIECE)
+            new_value = minimax_pruning(temp, depth - 1, alpha, beta,True)[1]
+            if new_value < value:
+                value = new_value
+                column = col
+            beta = min(beta, value)
+            if alpha >= beta:
+                break
+        return column, value
+
+
 
 
 def draw_board(board):
@@ -123,7 +276,6 @@ def draw_board(board):
 
 # #######################################################################################################################################################################################################################################################
 board = create_connect4()
-flip_board(board)
 
 # root = Tk()
 #
@@ -170,7 +322,7 @@ pygame.display.update()
 myfont = pygame.font.SysFont("monospace", 40)
 turn = random.randint(PLAYER, AI)
 
-while not game_done():
+while not game_done(board):
 
     draw_board(board)
 
@@ -194,7 +346,7 @@ while not game_done():
                     row = which_ROW(board, col)
                     piece_drop(board, row, col, PLAYER_PIECE)
 
-                    if game_done():
+                    if game_done(board):
                         s1, s2 = final_score(board)
                         if s1 > s2:
                             label = myfont.render(f"HUMAN wins!! {s1} - {s2}", 1, RED)
@@ -211,7 +363,7 @@ while not game_done():
                     flip_board(board)
                     draw_board(board)
                     turn = AI
-                    if game_done():
+                    if game_done(board):
                         pygame.time.wait(5000)
 
                 else:
@@ -219,15 +371,15 @@ while not game_done():
                     screen.blit(label, (10, 10))
                     continue
 
-    if turn == AI and not game_done():
+    if turn == AI and not game_done(board):
 
-        col = random.randint(0, COL_COUNT-1)
+        col, minimax_score = minimax_pruning(board, 4, -math.inf, math.inf, True)
 
         if move_is_valid(board, col):
             row = which_ROW(board, col)
             piece_drop(board, row, col, AI_PIECE)
 
-            if game_done():
+            if game_done(board):
                 s1, s2 = final_score(board)
                 if s1 > s2:
                     label = myfont.render(f"HUMAN wins!! {s1} - {s2}", 1, RED)
@@ -244,7 +396,7 @@ while not game_done():
             flip_board(board)
             draw_board(board)
             turn = PLAYER
-            if game_done():
+            if game_done(board):
                 pygame.time.wait(5000)
         else:
             continue
